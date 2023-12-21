3ab6d94130d7ba2d974059cc429aa000c7861b6c
shoroqsalameh <94695708+shoroqsalameh@users.noreply.github.com>
2023-12-21 09:54:35 +0300

    Update Add-Traineers.js

--- JS/Add-Traineers.js
+++ JS/Add-Traineers.js
@@ -1,48 +1,111 @@
 
-function createAccount(event) {
-    event.preventDefault();
 
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Check if there is no data stored before displaying the initial load alert
+    if (!localStorage.getItem('user_')) {
+        showAlert('All fields are required');
+    }
+
+    // Form submission
+    document.querySelector('form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        checkForm();
+    });
+});
+
+function checkForm() {
     var fullName = document.getElementById('fullname').value.trim();
     var email = document.getElementById('email').value.trim();
     var password = document.getElementById('password').value.trim();
+    var experience = document.getElementById('YearsOfExp').value.trim();
+    var areaOfExpertise = document.getElementById('AreaofExpertis').value.trim();
 
-    var alertMessage = document.getElementById('alert');
-    var alertContent = "";
-    var isValid = true;
-
-    // Basic email validation using regex
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!fullName || !email || !password) {
-        isValid = false;
-        alertContent = "All fields are required";
-    } else if (!emailRegex.test(email)) {
-        isValid = false;
-        alertContent = "Invalid email address";
+    // Show alert for empty fields
+    if (!fullName || !email || !password || !experience || !areaOfExpertise) {
+        showAlert('All fields are required');
+    } else if (!isValidEmail(email)) {
+        showAlert('Invalid email address');
+    } /*else if (!isValidPassword(password)) {
+        showAlert('Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one special character');
+    }*/ else if (!isValidExperience(experience)) {
+        showAlert('Years of experience must be a valid number.');
+    } else if (isEmailExists(email)) {
+        showAlert('Email already exists. Please choose a different email.');
     } else {
-        // Check if the email already exists in localStorage
-        if (isEmailExists(email)) {
-            isValid = false;
-            alertContent = "Email already exists. Please choose a different email.";
-        } else {
-            // Save the user information to localStorage
-            saveUserToLocalStorage(email, password);
-            alertContent = "Account created successfully!";
-       
-        }
+        // Assuming successful validation, proceed to create
+        create();
+         document.getElementById('fullname').value=" ";
+        document.getElementById('email').value=" ";
+        document.getElementById('password').value=" ";
+       document.getElementById('YearsOfExp').value=" ";
+     document.getElementById('AreaofExpertis').value=" ";
+     displayStoredData(); // Call the function to display stored data after successful registration
+        Swal.fire({
+            icon: 'success',
+            title: 'Success',
+            text: 'Added successfully',
+            confirmButtonText: 'OK'});
     }
+}
 
-    if (!isValid) {
-        alertMessage.innerHTML = alertContent;
-    }
+function isValidEmail(email) {
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+
+/*function isValidPassword(password) {
+    // Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one digit
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test(password);
+}*/
+
+
+function isValidExperience(experience) {
+    var experienceRegex = /^\d+$/;
+    return experienceRegex.test(experience);
 }
 
 function isEmailExists(email) {
-    return localStorage.getItem(email) !== null;
+    return localStorage.getItem('user_' + email) !== null;
 }
 
-function saveUserToLocalStorage(email, password) {
-    localStorage.setItem(email, password);
-    
+function create() {
+    var fullName = document.getElementById('fullname').value.trim();
+    var email = document.getElementById('email').value.trim();
+    var password = document.getElementById('password').value.trim();
+    var experience = document.getElementById('YearsOfExp').value.trim();
+    var areaOfExpertise = document.getElementById('AreaofExpertis').value.trim();
+
+    var dataObj = {
+        fullName: fullName,
+        email: email,
+        password: password,
+        experience: experience,
+        areaOfExpertise: areaOfExpertise
+    };
+
+    // Retrieve existing data from localStorage
+    var existingData = JSON.parse(localStorage.getItem('user_')) || [];
+
+    // Push new data to the array
+    existingData.push(dataObj);
+
+    // Save the updated array back to localStorage
+    localStorage.setItem('user_', JSON.stringify(existingData));
 }
 
+function showAlert(message) {
+    Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: message,
+        confirmButtonText: 'OK'
+    });
+}
+
+function logout() {
+    localStorage.clear();
+    window.location.replace('loginPage.html');
+}
